bfc5f224a402f132adf619dbb938103bedbb7c51
tinilijas <tinilij09@gmail.com>
2016-12-28 16:14:24 +0100

    pls work now :(

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,4 +4,14 @@ class HomepageController < ApplicationController
   def index
     @images = Image.all
   end
+
+  def create_image
+  image = StringIO.new(Base64.decode64(params[:image].tr(' ', '+')))
+  image.class.class_eval { attr_accessor :original_filename, :content_type }
+  image.original_filename = SecureRandom.hex + '.png'
+  image.content_type = 'image/png'
+
+  create_image = Image.new(image: image)
+  create_image.save!
+end
 end
