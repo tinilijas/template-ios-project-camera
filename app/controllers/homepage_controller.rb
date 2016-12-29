b90ed04837e3412b4f39b28235ea206bdd25becc
tinilijas <tinilij09@gmail.com>
2016-12-29 14:10:11 +0100

    finishing template

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -5,13 +5,9 @@ class HomepageController < ApplicationController
     @images = Image.all
   end
 
-  def create_image
-  image = StringIO.new(Base64.decode64(params[:image].tr(' ', '+')))
-  image.class.class_eval { attr_accessor :original_filename, :content_type }
-  image.original_filename = SecureRandom.hex + '.png'
-  image.content_type = 'image/png'
-
-  create_image = Image.new(image: image)
-  create_image.save!
-end
+  def delete_image
+    image = Image.find(params[:image])
+    image.destroy!
+    redirect_to :back
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'homepage#index'
-
-  post '/create-image' => 'homepage#create_image'
+  delete '/delete-image' => 'homepage#delete_image'
 end
